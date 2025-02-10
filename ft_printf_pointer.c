242d243943f7cb02866de598c3bfbdc6d950bc8c
James Albert Postadan <japostad@cbr4s5.42barcelona.com>
2025-02-10 17:34:47 +0100

    Fixed return value in %p when null is passed

--- ft_printf_pointer.c
+++ ft_printf_pointer.c
@@ -6,7 +6,7 @@
 /*   By: japostad <japostad@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 19:52:46 by japostad          #+#    #+#             */
-/*   Updated: 2025/01/13 19:52:51 by japostad         ###   ########.fr       */
+/*   Updated: 2025/02/10 17:28:13 by japostad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,21 @@ int	ft_printf_pointer(char const format, va_list ap, int *len)
 	char				*to_write;
 
 	p = va_arg(ap, unsigned long long);
-	to_write = ft_utoa_base(p, ft_baselen(format));
-	if (!to_write)
+	if((void *)p == NULL)
 	{
-		(*len) = -1;
-		return (*len);
+		write (1, "(nil)", 5);
+		*len +=5;
 	}
-	ft_printf_putnbr(format, to_write, len);
+	else
+	{
+		to_write = ft_utoa_base(p, ft_baselen(format));
+		if (!to_write)
+		{
+			(*len) = -1;
+			return (*len);
+		}
+		ft_printf_putnbr(format, to_write, len);
+	}
 	free(to_write);
 	return (*len);
 }
